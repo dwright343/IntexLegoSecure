444c36b09de4c66cbfe0158c8b444ce2338ed08e
vickip99 <131616765+vickip99@users.noreply.github.com>
2024-04-11 19:38:33 -0600

    filtering kind of.....

--- Views/Shared/Components/ProjectTypesViewComponent.cs
+++ Views/Shared/Components/ProjectTypesViewComponent.cs
@@ -1,22 +1,31 @@
 using IntexLegoSecure.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
-namespace IntexLegoSecure.Views.Shared.Components;
-
-public class ProjectTypesViewComponent : ViewComponent
+namespace IntexLegoSecure.Views.Shared.Components
 {
-    private I_Repository _repo;
-    //constructor (method ran once when building app)
-    public ProjectTypesViewComponent(I_Repository temp)
+    public class ProjectTypesViewComponent : ViewComponent
     {
-        _repo = temp;
-    }
-    public string Invoke()
-    {
-        var legoTypes = _repo.Products
-            .Select(x => x.Category)
-            .Distinct()
-            .OrderBy(x => x);
-        return ("");
+        private readonly I_Repository _repo;
+
+        // Constructor
+        public ProjectTypesViewComponent(I_Repository temp)
+        {
+            _repo = temp;
+        }
+
+        public string Invoke()
+        {
+            var legoTypes = _repo.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+
+            // Join the distinct category strings into a single string
+            var joinedCategories = string.Join(", ", legoTypes);
+
+            // Pass the joined string as the model to the view
+            return(joinedCategories);
+        }
     }
 }
