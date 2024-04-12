22e43d734afe6ec29e5422bd1bd187c430db542b
Isaac Day <dayisaacm81@gmail.com>
2024-04-11 22:33:17 -0600

    start onnx

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using IntexLegoSecure.ViewModels;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using System.Security.Cryptography.Xml;
 
 
 namespace IntexLegoSecure.Controllers
@@ -18,6 +21,8 @@ namespace IntexLegoSecure.Controllers
         //    _logger = logger;
         //}
 
+        private readonly InferenceSession _session;
+
         public IActionResult Index()
         {
             return View();
@@ -28,6 +33,75 @@ namespace IntexLegoSecure.Controllers
         public HomeController(I_Repository temp)
         {
             _repo = temp;
+
+            try
+            {
+                _session = new InferenceSession("C:\\Users\\dayis\\source\\repos\\IntexLegoSecure\\model.onnx");
+            }
+            catch (Exception ex) 
+            {
+
+            }
+        }
+
+        public IActionResult PredictFraud()
+        {
+            var record = new Order customerOrder();
+            
+            var fraud_dict = new Dictionary<int, string>
+            {
+                {0, "Not fraud" },
+                {1, "Fraud" }
+            };
+
+            var input = new List<float>
+            {
+                (float)record.Time,
+                (float)record.Amount,
+
+                record.DayOfWeek == "Mon" ? 1 : 0,
+                record.DayOfWeek == "Sat" ? 1 : 0,
+                record.DayOfWeek == "Sun" ? 1 : 0,
+                record.DayOfWeek == "Thu" ? 1 : 0,
+                record.DayOfWeek == "Tue" ? 1 : 0,
+                record.DayOfWeek == "Wed" ? 1 : 0,
+
+                record.EntryMode == "PIN" ? 1 : 0,
+                record.EntryMode == "Tap" ? 1 : 0,
+
+                record.TransactionType == "Online" ? 1 : 0,
+                record.TransactionType == "POS" ? 1 : 0,
+                record.TransactionType == "Online" ? 1 : 0,
+
+                record.TransactionCountry == "India" ? 1 : 0,
+                record.TransactionCountry == "Russia" ? 1 : 0,
+                record.TransactionCountry == "USA" ? 1 : 0,
+                record.TransactionCountry == "UnitedKingdom" ? 1 : 0,
+
+                record.ShippingCountry == "India" ? 1 : 0,
+                record.ShippingCountry == "Russia" ? 1 : 0,
+                record.ShippingCountry == "USA" ? 1 : 0,
+                record.ShippingCountry == "UnitedKingdom" ? 1 : 0,
+
+                record.Bank == "HSBC" ? 1 : 0,
+                record.Bank == "Halifax" ? 1 : 0,
+                record.Bank == "Lloyds" ? 1 : 0,
+                record.Bank == "Metro" ? 1 : 0,
+                record.Bank == "Monzo" ? 1 : 0,
+                record.Bank == "RBS" ? 1 : 0,
+
+                record.CardType == "Visa" ? 1 : 0
+            };
+
+            var inputTensor = new DenseTensor<float>(input.ToArray(), new[] { 1, input.Count });
+
+            var inputs = new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("float_input", inputTensor)
+            };
+
+
+            
         }
 
         public IActionResult ListProducts(string? primaryColor, int pageNum = 1) // name this pageNum, because "page" means something to the .NET environment
