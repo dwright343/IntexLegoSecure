7118d1bce3669931cefcc8400677ca2b24a6b0cc
vickip99 <131616765+vickip99@users.noreply.github.com>
2024-04-12 08:25:22 -0600

    recommended

---
+++ ViewModels/DetailedProductViewModel.cs
@@ -0,0 +1,7 @@
+namespace IntexLegoSecure.ViewModels;
+
+public class DetailedProductViewModel
+{
+    public Product MainProduct { get; set; }
+    public List<Product> RecommendedProducts { get; set; }
+}
